7336419d6cda1b6ae1dde848e0a4622a98eb8a49
Filipek Paweł <filipek@regis1.com.pl>
2017-05-13 15:20:18 +0200

    fp

---
+++ src/SendClassFromView/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SendClassFromView.Model;
+
+namespace SendClassFromView.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Values([FromBody] SampleClass[] model)
+        {
+            return Json(new { someValue = "It's ok" });
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
